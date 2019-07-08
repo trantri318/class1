9ef3051df3cb31f0b7599346c84c62c2654fae15
“Tri” <trantrilong318@gmail.com>
2019-07-08 10:26:00 +0700

    Initial commit

---
+++ db/migrate/20190708032243_create_student.rb
@@ -0,0 +1,11 @@
+class CreateStudent < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :class_university
+      t.float :score
+      t.text :learning_power
+      t.timestamps
+    end
+  end
+end
